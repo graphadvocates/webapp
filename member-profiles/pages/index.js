200937c0df85d73a0f48253ccd801ad8d3c67689
dylviz <dylanvizcarra@gmail.com>
2022-07-09 17:48:49 -0700

    Updated components for vercel

--- member-profiles/pages/index.js
+++ member-profiles/pages/index.js
@@ -12,16 +12,16 @@ export default function Home() {
   return (
     <Box>
       <VStack>
-        <Image maxW={1500} src="/assets/banner.jpeg"/>
+        <Image maxW={1500} alt="Home banner" src="/assets/banner.jpeg"/>
 
         <HStack maxW={1500}>
           <Heading p="5">
             Open community updates supporting the Graph Protocol and Web3 community
           </Heading>
-          <Image src="/assets/CommunityTalk.jpg"/>
+          <Image alt="Community Talk banner" src="/assets/CommunityTalk.jpg"/>
         </HStack>
         
-        <Image src="/assets/AdvocateDAO@2x.png"/>
+        <Image alt="advocateDAO banner" src="/assets/AdvocateDAO@2x.png"/>
 
         <SmallWithSocial />
 
