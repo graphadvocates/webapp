e48223f2a08d07d727c6baa887ed42d6c6b485a7
dylviz <dylanvizcarra@gmail.com>
2022-08-15 15:28:44 -0700

    ok for fuck sake

--- member-profiles/pages/daoresources/index.js
+++ member-profiles/pages/daoresources/index.js
@@ -26,10 +26,10 @@ export default function daoresources() {
                 </TabList>
 
                 <TabPanels>
-                    <TabPanel><iframe className="clickup-embed clickup-dynamic-height" src="https://forms.clickup.com/37437860/f/13pgd4-4007/RXO7DCQPT5XCA8X7R7" onwheel="" width="100%" height="100%"></iframe><script async src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"></script></TabPanel>
-                    <TabPanel><iframe className="clickup-embed clickup-dynamic-height" src="https://forms.clickup.com/37437860/f/13pgd4-3987/1HEIW31922QZ1CRS64" onwheel="" width="100%" height="100%"></iframe><script async src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"></script></TabPanel>
-                    <TabPanel><iframe className="clickup-embed clickup-dynamic-height" src="https://forms.clickup.com/37437860/f/13pgd4-7227/SPBI1F0O1M7LXTL5LH" onwheel="" width="100%" height="100%"></iframe><script async src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"></script></TabPanel>
-                    <TabPanel><iframe className="clickup-embed clickup-dynamic-height" src="https://forms.clickup.com/37437860/f/13pgd4-7140/XHKARLTPHSZ7JBMVD2" onwheel="" width="100%" height="100%"></iframe><script async src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"></script></TabPanel>
+                    <TabPanel><iframe className="clickup-embed clickup-dynamic-height" src="https://forms.clickup.com/37437860/f/13pgd4-4007/RXO7DCQPT5XCA8X7R7" onWheel="" width="100%" height="100%"></iframe><script async src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"></script></TabPanel>
+                    <TabPanel><iframe className="clickup-embed clickup-dynamic-height" src="https://forms.clickup.com/37437860/f/13pgd4-3987/1HEIW31922QZ1CRS64" onWheel="" width="100%" height="100%"></iframe><script async src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"></script></TabPanel>
+                    <TabPanel><iframe className="clickup-embed clickup-dynamic-height" src="https://forms.clickup.com/37437860/f/13pgd4-7227/SPBI1F0O1M7LXTL5LH" onWheel="" width="100%" height="100%"></iframe><script async src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"></script></TabPanel>
+                    <TabPanel><iframe className="clickup-embed clickup-dynamic-height" src="https://forms.clickup.com/37437860/f/13pgd4-7140/XHKARLTPHSZ7JBMVD2" onWheel="" width="100%" height="100%"></iframe><script async src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"></script></TabPanel>
                 </TabPanels>
             </Tabs>
         </Box>
