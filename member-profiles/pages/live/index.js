8de4e117c963323c8a99c5a17189bdb4a1f63923
dylviz <dylanvizcarra@gmail.com>
2022-09-05 12:33:52 -0700

    removed livestream

--- member-profiles/pages/live/index.js
+++ member-profiles/pages/live/index.js
@@ -1,39 +1,5 @@
-import { Flex } from "@chakra-ui/react";
-import { useEffect, useRef } from "react";
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
+import LiveStream from "../../components/LiveStream";
 
 export default function live() {
-    const videoRef = useRef(null);
-
-    useEffect(() => {
-        if (videoRef.current) {
-            videojs(videoRef.current, {
-                sources: [
-                    {
-                        src: "rtmp://rtmp.livepeer.com/live",
-                        type: "application/x-mpegURL",
-                    },
-                ],
-            });
-        }
-    });
-
-    return (
-        <div>
-            <video
-                controls
-                ref={videoRef}
-                className="video-js vjs-big-play-centered"
-            />
-
-            <iframe
-                src="https://lvpr.tv?v=53e6-w2yw-35pf-ko4x"
-                frameBorder="0"
-                allowfullscreen
-                allow="autoplay; encrypted-media; picture-in-picture"
-                sandbox="allow-scripts"
-            ></iframe>
-        </div>
-    );
+    return <LiveStream />;
 }
