8ebb5fd13b67bdd6fd8540be8bd4f233d3ef4317
dylviz <dylanvizcarra@gmail.com>
2023-04-12 15:46:24 -0700

    Added Carousel for landing page

--- README.md
+++ README.md
@@ -6,5 +6,12 @@ Create a local `.env` file and add the following variable `CLICKUP_API_KEY=""` a
 This file will be gitignored and should not be pushed or shared with anyone.
 If you don't have a Clickup API key with the appropriate permissions, you should still be able to run the application locally. Pages that utilize the ClickUp API
 will appear empty.
- 
+
 To run use `yarn dev`
+
+## Modify the carousel
+
+1. `public > assets > LandingCarousel` will hold all images that will be used in the carousel.
+2. Images are shown in the order of the name of the file (e.g. 1.jpg, 2.jpg, etc..)
+3. Image recommendation are to maintain a max aspect ration of **2:1** (e.g. 2:1 and 1:1 will render perfectly). Anything beyond that will render blury
+4. Image will be renedered with a max height of **600px** (The image can be any size as long as it maintains appropriate ratios)
--- pages/index.js
+++ pages/index.js
@@ -1,126 +1,145 @@
 import {
-    Box,
-    Heading,
-    useColorModeValue,
-    Image,
-    VStack,
-    HStack,
-    Button,
-    Icon,
-    Arrow,
-    Text,
-    Flex,
-    Divider,
-    Link,
-} from "@chakra-ui/react";
+  Box,
+  Heading,
+  useColorModeValue,
+  Image,
+  VStack,
+  HStack,
+  Button,
+  Icon,
+  Arrow,
+  Text,
+  Flex,
+  Divider,
+  Link,
+} from '@chakra-ui/react';
 
-import RolesComp from "../components/RolesComp";
+import RolesComp from '../components/RolesComp';
+import Carousel from 'framer-motion-carousel';
 
 export default function Home() {
-    return (
-        <Flex justifyContent={"center"} p={5}>
-            <VStack maxW={"1200px"}>
-                <Box color={"whiteAlpha.900"} m={2} pb={20}>
-                    <Text textStyle="h2" align={"center"}>
-                        Graph
-                    </Text>
-                    <Text as="b" textStyle="h1" fontFamily={"heading"}>
-                        Advocates
-                    </Text>
-                </Box>
+  return (
+    // <Flex justifyContent={'center'} p={5}>
+    <VStack border={'1px'} borderColor={'green.300'}>
+      <Box color={'whiteAlpha.900'} m={2} p={4}>
+        <Text textStyle="h2" align={'center'}>
+          Graph
+        </Text>
+        <Text fontWeight={800} textStyle="h1" fontFamily={'heading'}>
+          Advocates
+        </Text>
+      </Box>
 
-                <VStack
-                    borderTop={"4px"}
-                    borderBottom={"4px"}
-                    borderRadius={"xl"}
-                    borderColor={"blue.200"}
-                    p={"8"}
-                    pt={"14"}
-                    pb={"8"}
-                >
-                    <Text align={"center"} textStyle={"h3"}>
-                        Advocates focus on different roles in the community
-                        based on their interests and abilities.
-                    </Text>
-                    <Link
-                        href="https://forms.clickup.com/37437860/f/13pgd4-4007/RXO7DCQPT5XCA8X7R7"
-                        isExternal
-                    >
-                        <Button
-                            border="2px"
-                            borderColor="blue.200"
-                            colorScheme={"blue"}
-                            m={8}
-                        >
-                            Apply
-                        </Button>
-                    </Link>
-                </VStack>
+      <Box
+        maxW={1200}
+        bg={'blackAlpha.400'}
+        borderRadius={'3xl'}
+        overflow={'hidden'}
+      >
+        <Carousel interval={3000}>
+          {[1, 2, 3, 4].map((item, i) => (
+            <Flex key={i} justify={'center'}>
+              <Image
+                draggable={'false'}
+                src={'/assets/LandingCarousel/' + item + '.jpg'}
+                alt="pic"
+                maxH={600}
+              />
+            </Flex>
+          ))}
+        </Carousel>
+      </Box>
 
-                <VStack>
-                    <Text pt={20} mb={"-6"} textStyle="h2">
-                        How It Works
-                    </Text>
-                    <Image
-                        pb={20}
-                        alt="advocateDAO banner"
-                        src="/assets/InfoGraphics/AdvocateDAO@2x2.png"
-                    />
-                </VStack>
+      <VStack
+        borderTop={'4px'}
+        borderBottom={'4px'}
+        borderRadius={'xl'}
+        borderColor={'blue.200'}
+        p={'8'}
+        pt={'14'}
+        pb={'8'}
+      >
+        <Text align={'center'} textStyle={'h3'}>
+          Advocates focus on different roles in the community based on their
+          interests and abilities.
+        </Text>
+        <Link
+          href="https://forms.clickup.com/37437860/f/13pgd4-4007/RXO7DCQPT5XCA8X7R7"
+          isExternal
+        >
+          <Button
+            border="2px"
+            borderColor="blue.200"
+            colorScheme={'blue'}
+            m={8}
+          >
+            Apply
+          </Button>
+        </Link>
+      </VStack>
 
-                <VStack
-                    p={"8"}
-                    pt={"14"}
-                    pb={"14"}
-                    borderTop={"4px"}
-                    borderBottom={"4px"}
-                    borderRadius={"xl"}
-                    borderColor={"blue.200"}
-                >
-                    <Text textStyle="h2">Community Talk</Text>
-                    <Text p="5" textStyle="h3">
-                        Host community updates for the Graph Protocol and Web3
-                        community!
-                    </Text>
-                </VStack>
+      <VStack>
+        <Text pt={20} mb={'-6'} textStyle="h2">
+          How It Works
+        </Text>
+        <Image
+          pb={20}
+          alt="advocateDAO banner"
+          src="/assets/InfoGraphics/AdvocateDAO@2x2.png"
+        />
+      </VStack>
 
-                <Box pt={28} mb={10}>
-                    <Text textStyle="h2">Advocate Roles</Text>
-                </Box>
+      <VStack
+        p={'8'}
+        pt={'14'}
+        pb={'14'}
+        borderTop={'4px'}
+        borderBottom={'4px'}
+        borderRadius={'xl'}
+        borderColor={'blue.200'}
+      >
+        <Text textStyle="h2">Community Talk</Text>
+        <Text p="5" textStyle="h3">
+          Host community updates for the Graph Protocol and Web3 community!
+        </Text>
+      </VStack>
 
-                <RolesComp />
+      <Box pt={28} mb={10}>
+        <Text textStyle="h2">Advocate Roles</Text>
+      </Box>
 
-                <VStack
-                    p={"8"}
-                    pt={"14"}
-                    pb={"14"}
-                    borderTop={"4px"}
-                    borderBottom={"4px"}
-                    borderRadius={"xl"}
-                    borderColor={"blue.200"}
-                >
-                    <Text textStyle="h2">Community Grants</Text>
-                    <Text p="5" textStyle="h3">
-                        Want to support the Graph community? We want to support
-                        you! Apply for a community grant today!
-                    </Text>
+      <RolesComp />
 
-                    <Link
-                        href="https://forms.clickup.com/37437860/f/13pgd4-3987/1HEIW31922QZ1CRS64"
-                        isExternal
-                    >
-                        <Button
-                            border="2px"
-                            borderColor="blue.200"
-                            colorScheme={"blue"}
-                            m={8}
-                        >
-                            Apply for Grant
-                        </Button>
-                    </Link>
+      <VStack
+        p={'8'}
+        pt={'14'}
+        pb={'14'}
+        borderTop={'4px'}
+        borderBottom={'4px'}
+        borderRadius={'xl'}
+        borderColor={'blue.200'}
+      >
+        <Text textStyle="h2">Community Grants</Text>
+        <Text p="5" textStyle="h3">
+          Want to support the Graph community? We want to support you! Apply for
+          a community grant today!
+        </Text>
 
-                </VStack>
-            </VStack>
-        </Flex>
-    );
+        <Link
+          href="https://forms.clickup.com/37437860/f/13pgd4-3987/1HEIW31922QZ1CRS64"
+          isExternal
+        >
+          <Button
+            border="2px"
+            borderColor="blue.200"
+            colorScheme={'blue'}
+            m={8}
+          >
+            Apply for Grant
+          </Button>
+        </Link>
+      </VStack>
+    </VStack>
+    // </Flex>
+  );
 }
