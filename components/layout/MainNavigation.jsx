95811308c5715522ef240ef9afd6cff02dc63d5a
dylviz <dylanvizcarra@gmail.com>
2023-05-22 10:09:21 -0700

    Updates Header link with community grant link

--- components/layout/MainNavigation.jsx
+++ components/layout/MainNavigation.jsx
@@ -1,131 +1,168 @@
 import {
-  Box,
-  Flex,
-  HStack,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-  Img,
-  Text,
-  Link,
-} from '@chakra-ui/react';
-import NavLink from './NavLink';
-import NextLink from 'next/link';
-import { HamburgerIcon, CloseIcon, Search2Icon } from '@chakra-ui/icons';
+	Box,
+	Flex,
+	HStack,
+	IconButton,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+	useDisclosure,
+	useColorModeValue,
+	Stack,
+	Img,
+	Text,
+	Link,
+	Divider,
+} from "@chakra-ui/react";
+import NavLink from "./NavLink";
+import NextLink from "next/link";
+import {
+	HamburgerIcon,
+	CloseIcon,
+	Search2Icon,
+	ChevronDownIcon,
+	ChevronUpIcon,
+} from "@chakra-ui/icons";
 
 export default function MainNavigation() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const {
+		isOpen: applyIsOpen,
+		onOpen: applyOnOpen,
+		onClose: applyOnClose,
+	} = useDisclosure();
 
-  const Links = [
-    // { name: 'Dashboard', path: '/dashboard' },
-    // {
-    //     name: "Team",
-    //     path: "/",
-    //     subLinks: [
-    //         {
-    //             name: "Roles",
-    //             path: "/roles",
-    //         },
-    //         {
-    //             name: "Members",
-    //             path: "/daomembers",
-    //         },
-    //         {
-    //             name: "Advocates",
-    //             path: "/advocates",
-    //         },
-    //     ],
-    // },
-    { name: 'Docs', path: 'https://docs.graphadvocates.com/' },
-    { name: 'Forum', path: 'https://forum.graphadvocates.com/' },
+	const Links = [
+		// { name: 'Dashboard', path: '/dashboard' },
+		// {
+		//     name: "Team",
+		//     path: "/",
+		//     subLinks: [
+		//         {
+		//             name: "Roles",
+		//             path: "/roles",
+		//         },
+		//         {
+		//             name: "Members",
+		//             path: "/daomembers",
+		//         },
+		//         {
+		//             name: "Advocates",
+		//             path: "/advocates",
+		//         },
+		//     ],
+		// },
+		{ name: "Docs", path: "https://docs.graphadvocates.com/" },
+		{ name: "Forum", path: "https://forum.graphadvocates.com/" },
+	];
 
-    // { name: 'Live', path: '/live' },
-  ];
+	return (
+		<Box
+			bg={useColorModeValue("rgba(12,10,29,1.0)", "blue.900")}
+			px={4}
+			sx={{
+				position: "-webkit-sticky",
+				/* Safari */ position: "sticky",
+				top: "0",
+				zIndex: "1",
+			}}
+			opacity={0.95}
+		>
+			<Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+				{/* Side Bar Mobile Drop-Down Navigation */}
+				<Menu>
+					<MenuButton
+						as={IconButton}
+						aria-label="Options"
+						icon={<HamburgerIcon />}
+						variant="outline"
+						// size={"md"}
+						// icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+						// aria-label={"Open Menu"}
+						display={{ md: "none" }}
+						//onClick={isOpen ? onClose : onOpen}
+					></MenuButton>
+					<MenuList>
+						<Text>test</Text>
+					</MenuList>
+				</Menu>
 
-  return (
-    <Box
-      bg={useColorModeValue('rgba(12,10,29,1.0)', 'blue.900')}
-      px={4}
-      sx={{
-        position: '-webkit-sticky',
-        /* Safari */ position: 'sticky',
-        top: '0',
-        zIndex: '1',
-      }}
-      opacity={0.95}
-    >
-      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-        <Menu>
-          <MenuButton
-            as={IconButton}
-            aria-label="Options"
-            icon={<HamburgerIcon />}
-            variant="outline"
-            // size={"md"}
-            // icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            // aria-label={"Open Menu"}
-            display={{ md: 'none' }}
-            //onClick={isOpen ? onClose : onOpen}
-          ></MenuButton>
-          <MenuList>
-            <Text>test</Text>
-          </MenuList>
-        </Menu>
+				{/* Main Navigation Header */}
+				<HStack spacing={8} alignItems={"center"}>
+					<NextLink href="/">
+						<Img cursor={"pointer"} src={"/assets/Icons/graphIcon.svg"} />
+					</NextLink>
+					<NavLink LinkList={Links} />
+				</HStack>
 
-        <HStack spacing={8} alignItems={'center'}>
-          <NextLink href="/">
-            <Img cursor={'pointer'} src={'/assets/Icons/graphIcon.svg'} />
-          </NextLink>
+				<Flex alignItems={"center"}>
+					{/* <Link
+						href="https://forms.clickup.com/37437860/f/13pgd4-4007/RXO7DCQPT5XCA8X7R7"
+						isExternal
+					>
+						<Button
+							border="2px"
+							borderColor="blue.200"
+							colorScheme={"blue"}
+							fontSize={"18"}
+							m={8}
+							p={4}
+						>
+							Apply Here!
+						</Button>
+					</Link> */}
 
-          <NavLink LinkList={Links} />
-        </HStack>
+					<Menu>
+						<MenuButton
+							as={IconButton}
+							onClick={applyIsOpen ? applyOnClose : applyOnOpen}
+							aria-label="Apply"
+							border="2px"
+							borderColor="blue.200"
+							colorScheme={"blue"}
+							fontSize={"18"}
+							m={8}
+							p={4}
+						>
+							{" "}
+							Apply Here!
+							{applyIsOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+						</MenuButton>
+						<MenuList>
+							<Link
+								href="https://forms.clickup.com/37437860/f/13pgd4-4007/RXO7DCQPT5XCA8X7R7"
+								isExternal
+							>
+								<MenuItem
+									color={"blackAlpha.900"}
+									justifyContent={"center"}
+									fontWeight={"700"}
+								>
+									Become an Advocate!
+								</MenuItem>
+							</Link>
+							<Divider />
+							<Link
+								href="https://forms.clickup.com/37437860/f/13pgd4-3987/1HEIW31922QZ1CRS64"
+								isExternal
+							>
+								<MenuItem
+									color={"blackAlpha.900"}
+									justifyContent={"center"}
+									fontWeight={"700"}
+								>
+									Community Grant
+								</MenuItem>
+							</Link>
+						</MenuList>
+					</Menu>
+				</Flex>
+			</Flex>
 
-        <Flex alignItems={'center'}>
-          <Link
-            href="https://forms.clickup.com/37437860/f/13pgd4-4007/RXO7DCQPT5XCA8X7R7"
-            isExternal
-          >
-            <Button
-              border="2px"
-              borderColor="blue.200"
-              colorScheme={'blue'}
-              fontSize={'18'}
-              m={8}
-              p={4}
-            >
-              Apply Here!
-            </Button>
-          </Link>
-
-          {/* <Search2Icon /> */}
-          {/* <Menu>
-                        <MenuButton
-                            as={Button}
-                            rounded={"full"}
-                            variant={"link"}
-                            cursor={"pointer"}
-                            minW={0}
-                        >
-                            <Search2Icon />
-                        </MenuButton>
-                        <MenuList>
-                            <MenuItem>Link 1</MenuItem>
-                            <MenuItem>Link 2</MenuItem>
-                            <MenuDivider />
-                            <MenuItem>Link 3</MenuItem>
-                        </MenuList>
-                    </Menu> */}
-        </Flex>
-      </Flex>
-
-      {/* {isOpen ? (
+			{/* {isOpen ? (
                 <Box pb={4} display={{ md: "none" }}>
                     <Stack as={"nav"} spacing={4}>
                         {Links.map((link, idx) => (
@@ -134,6 +171,6 @@ export default function MainNavigation() {
                     </Stack>
                 </Box>
             ) : null} */}
-    </Box>
-  );
+		</Box>
+	);
 }
--- components/layout/NavLink.jsx
+++ components/layout/NavLink.jsx
@@ -1,84 +1,89 @@
 import {
-  Link,
-  HStack,
-  useColorModeValue,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  Button,
-  Box,
-  Flex,
-  useDisclosure,
-  textDecoration,
-} from '@chakra-ui/react';
-import { ChevronDownIcon, ChevronUpIcon, Search2Icon } from '@chakra-ui/icons';
-import NextLink from 'next/link';
+	Link,
+	HStack,
+	useColorModeValue,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+	Button,
+	Box,
+	Flex,
+	useDisclosure,
+	textDecoration,
+} from "@chakra-ui/react";
+import {
+	ChevronDownIcon,
+	ChevronUpIcon,
+	ExternalLinkIcon,
+	Search2Icon,
+} from "@chakra-ui/icons";
 
 export default function NavLink(props) {
-  const { isOpen, onOpen, onClose, onToggle } = useDisclosure();
+	const { isOpen, onOpen, onClose, onToggle } = useDisclosure();
 
-  return (
-    <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
-      {props.LinkList.map((link, index) => (
-        <Box
-          px={2}
-          py={1}
-          rounded={'md'}
-          _hover={{
-            textDecoration: 'none',
-            bg: 'gray.200',
-            color: 'black',
-          }}
-          color="gray.200"
-          key={index}
-        >
-          {link.hasOwnProperty('subLinks') ? (
-            <Flex alignItems={'center'}>
-              <Menu>
-                <MenuButton
-                  as={Button}
-                  rightIcon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-                  rounded={'full'}
-                  variant={'link'}
-                  cursor={'pointer'}
-                  minW={0}
-                  color={isOpen ? 'black' : 'gray.200'}
-                  _hover={{
-                    textDecoration: 'none',
-                    bg: 'gray.200',
-                    color: 'black',
-                  }}
-                >
-                  {link.name}
-                </MenuButton>
+	return (
+		<HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
+			{props.LinkList.map((link, index) => (
+				<Box
+					px={2}
+					py={1}
+					rounded={"md"}
+					_hover={{
+						textDecoration: "none",
+						bg: "gray.200",
+						color: "black",
+					}}
+					color="gray.200"
+					key={index}
+				>
+					{link.hasOwnProperty("subLinks") ? (
+						<Flex alignItems={"center"}>
+							<Menu>
+								<MenuButton
+									as={Button}
+									rightIcon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+									rounded={"full"}
+									variant={"link"}
+									cursor={"pointer"}
+									minW={0}
+									color={isOpen ? "black" : "gray.200"}
+									_hover={{
+										textDecoration: "none",
+										bg: "gray.200",
+										color: "black",
+									}}
+								>
+									{link.name}
+								</MenuButton>
 
-                <MenuList textDecoration="none">
-                  {link.subLinks.map((sLink, idx) => (
-                    <NextLink href={sLink.path} key={idx}>
-                      <MenuItem>{sLink.name}</MenuItem>
-                    </NextLink>
-                  ))}
-                </MenuList>
-              </Menu>
-            </Flex>
-          ) : (
-            <Link
-              href={link.path}
-              _hover={{
-                textDecoration: 'none',
-                bg: 'gray.200',
-                color: 'black',
-              }}
-              fontFamily={'Open Sans'}
-              fontWeight={'bold'}
-            >
-              {link.name}
-            </Link>
-          )}
-        </Box>
-      ))}
-    </HStack>
-  );
+								<MenuList textDecoration="none">
+									{link.subLinks.map((sLink, idx) => (
+										<Link href={sLink.path} key={idx} isExternal={true}>
+											<MenuItem>{sLink.name}</MenuItem>
+										</Link>
+									))}
+								</MenuList>
+							</Menu>
+						</Flex>
+					) : (
+						<Link
+							href={link.path}
+							_hover={{
+								textDecoration: "none",
+								bg: "gray.200",
+								color: "black",
+							}}
+							fontFamily={"Open Sans"}
+							fontWeight={"bold"}
+							isExternal
+						>
+							{link.name}
+						</Link>
+					)}
+				</Box>
+			))}
+		</HStack>
+	);
 }
